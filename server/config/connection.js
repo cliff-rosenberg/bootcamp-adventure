b2d86e8c992ecbccd58d5ff7037b6e9ea5f81185
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-09 20:02:52 -0700

    initial connection setup file

---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+// boilerplate for Mongoose ORM setup
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
