f26e467a9d26f99c0aec57c0736af64c51a89b39
Kevin <kldpd22@gmail.com>
2022-08-15 19:25:06 -0700

    worked on homepage and added questions component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 // import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import Question from "../components/Questions";
 
 const Home = () => {
   return (
     <div className="container">
         {/* <Header /> */}
         <Nav />
+
+          <Question />
+
         <Footer />
     </div>
   );
