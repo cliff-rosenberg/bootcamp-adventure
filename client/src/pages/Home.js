c6fcac530fe12517b17e4dc03e2223f788e679d1
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-17 11:44:40 -0700

    added code for page Auth

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 // import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import Question from "../components/Questions";
+
+// load the Auth setup for the page
+import Auth from '../utils/auth';
 
 const Home = () => {
   return (
     <div className="container">
         {/* <Header /> */}
         <Nav />
-
+        <div className="row">
+                        <div className="col-12 text-center mt-3">
+                            <img src="/images/Logo1.png" alt="large logo" className="img w-100"></img>
+                        </div>
+                    </div>
+        {Auth.loggedIn() ? (
           <Question />
-
+          ) : (
+        <p className="text-center">
+          You need to be logged in to play this game. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
         <Footer />
     </div>
   );
